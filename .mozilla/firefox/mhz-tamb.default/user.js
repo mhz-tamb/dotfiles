7d59d17b7d28c4da80cf758d6fe0c1f95a3d3e1c
Sergey Shiryaev <sergey@shiryaev.pro>
2015-11-21 16:32:13 +0300

    add firefox config

---
+++ .mozilla/firefox/mhz-tamb.default/user.js
@@ -0,0 +1,11 @@
+// disable Hello
+user_pref("loop.enabled", false);
+// disable Pocket
+user_pref("browser.pocket.enabled", false);
+
+// Move cache to ram
+user_pref("browser.cache.disk.enable", false);
+user_pref("browser.cache.memory.enable", true);
+
+user_pref("browser.urlbar.formatting.enabled", false);
+user_pref("browser.urlbar.trimURLs", false);
