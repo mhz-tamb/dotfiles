49e7da697eabd97b61d9e14a51dcede892540319
Sergey Shiryaev <sergey@shiryaev.pro>
2015-11-29 12:27:32 +0300

    more firefox settings

--- .mozilla/firefox/mhz-tamb.default/user.js
+++ .mozilla/firefox/mhz-tamb.default/user.js
@@ -3,9 +3,17 @@ user_pref("loop.enabled", false);
 // disable Pocket
 user_pref("browser.pocket.enabled", false);
 
-// Move cache to ram
+// move cache to ram
 user_pref("browser.cache.disk.enable", false);
 user_pref("browser.cache.memory.enable", true);
 
 user_pref("browser.urlbar.formatting.enabled", false);
 user_pref("browser.urlbar.trimURLs", false);
+
+// restore tabs
+user_pref("browser.startup.page", 3);
+// ask download dir
+user_pref("browser.download.useDownloadDir", false);
+
+user_pref("devtools.theme", "dark");
+user_pref("devtools.editor.keymap", "vim");
